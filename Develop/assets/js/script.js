d09ef51dcf9cd4f9fe6e68bd441ee7c0314b55a8
TheR16H <rashawn5hall@gmail.com>
2024-05-13 17:41:01 -0400

    notes

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,8 +4,13 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    const randomid = Math.random().toString(16).slice(2);
+    const uniqueId = 'id_' + randomid;
+    return uniqueId;
 }
+
+const newId = generateTaskId();
+console.log(newId);
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
@@ -24,12 +29,12 @@ function handleAddTask(event){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
+// Btnclicked $(event.target) format here i think 
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+//  sortable
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
