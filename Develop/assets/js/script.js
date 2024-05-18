a64fe842f8d5c74979a92a0b785bcdf1be81eb99
TheR16H <rashawn5hall@gmail.com>
2024-05-18 17:32:56 -0400

    modalup

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,11 +2,23 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+//code used to make the modal pop up and the btn work.
+document.getElementById('modalForm');
+const btn = document.querySelector('.btn.btn-success')
+const closeBtn = modal.querySelector('.close');
+btn.addEventListener('click', function(){
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function(){
+    modal.style.display = 'none';
+});
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     const randomid = Math.random().toString(16).slice(2);
     const uniqueId = 'id_' + randomid;
-    return uniqueId;
+    return uniqueId; 
 }
 
 const newId = generateTaskId();
@@ -14,8 +26,14 @@ console.log(newId);
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = $('<div>')
+        .addClass('card task-card draggable my-3')
+        .attr('taskId', task.id);
 
+    return taskCard;
 }
+
+
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
